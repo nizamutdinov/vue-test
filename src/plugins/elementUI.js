5f602f351fc2d3d9f1e436a398838cfe2ff64bb3
nizamutdinov <pinkyprotos@gmail.com>
2020-11-09 02:40:43 +0500

    feat: elementUI library added

---
+++ src/plugins/elementUI.js
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+import elementUI from 'element-ui';
+import locale from 'element-ui/lib/locale/lang/ru-RU';
+
+// TODO: import components we actually need
+// E.G.
+// Vue.use(Table)
+
+Vue.use(elementUI, { locale });
