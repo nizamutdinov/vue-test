de432b7c7cd44a8ee81231e3531814c72082c711
nizamutdinov <pinkyprotos@gmail.com>
2020-11-09 05:39:16 +0500

    feat: store persistent

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,21 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import createPersistedState from 'vuex-persistedstate';
 import { employees } from './modules/employees';
 
 Vue.use(Vuex);
 
-const index = new Vuex.Store({
+const store = new Vuex.Store({
   modules: { employees },
+  actions: {
+    init({ dispatch }) {
+      dispatch('employees/init');
+    },
+  },
+  plugins: [createPersistedState()],
 });
 
-export default index;
+// eslint-disable-next-line no-return-await
+(async () => await store.dispatch('init'))();
+
+export default store;
